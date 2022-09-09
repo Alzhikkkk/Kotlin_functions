5fe9db77187a09590eb21b00da381ec093b7ed22
Alzhikkkk <alzhikakh@gmail.com>
2022-09-09 17:21:49 +0600

    Expression in a string template

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,7 @@ fun main(args: Array<String>) {
     println("Hello World!")
 
     val temperature = 10
-    val isHot = if (temperature > 50) true else false
-    println(isHot)
+    val message = "The water temperature is ${ if (temperature > 50) "too warm" else "OK" }."
+    println(message)
 
 }
