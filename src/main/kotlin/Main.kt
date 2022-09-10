a4a037da720e689fa505badb7731514eff4f485d
Alzhikkkk <alzhikakh@gmail.com>
2022-09-10 14:15:42 +0600

    Creating a higher-order function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,11 @@
 import java.util.*;
 
+fun updateDirty(dirty: Int, operation: (Int) -> Int): Int {
+    return operation(dirty)
+}
 fun main() {
-    var dirtyLevel = 20
-    val waterFilter = { dirty : Int -> dirty / 2}
-    println(waterFilter(dirtyLevel))
+    val waterFilter: (Int) -> Int = { dirty -> dirty / 2 }
+    println(updateDirty(30, waterFilter))
 }
 
 
