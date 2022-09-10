4a0368294b82b72e8cd14c31a2c7cdc8c57f7ab4
Alzhikkkk <alzhikakh@gmail.com>
2022-09-10 14:00:34 +0600

    Forcing a sequence filter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,6 +6,8 @@ fun main() {
     // eager, creates a new list
     val filtered = decorations.asSequence().filter { it[0] == 'p' }
     println("filtered: $filtered")
+    val newList = filtered.toList()
+    println("new list: $newList")
 }
 
 
