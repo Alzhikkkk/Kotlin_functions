0b1c6c7e8c295617db749fa46ec116a308924e36
Alzhikkkk <alzhikakh@gmail.com>
2022-09-10 13:36:38 +0600

    Function with default value

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,28 +1,7 @@
-import java.util.*;
-fun fishFood (day : String) : String {
-    return when (day) {
-        "Monday" -> "flakes"
-        "Wednesday" -> "redworms"
-        "Thursday" -> "granules"
-        "Friday" -> "mosquitoes"
-        "Sunday" -> "plankton"
-        else -> "nothing"
-    }
-}
-
-fun feedTheFish() {
-    val day = randomDay()
-    val food = fishFood(day)
-
-    println ("Today is $day and the fish eat $food")
-}
-
 fun main(args: Array<String>) {
-    feedTheFish()
+    swim()
 }
 
-fun randomDay() : String {
-    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
-        "Friday", "Saturday", "Sunday")
-    return week[Random().nextInt(week.size)]
+fun swim(speed: String = "fast") {
+    println("swimming $speed")
 }
