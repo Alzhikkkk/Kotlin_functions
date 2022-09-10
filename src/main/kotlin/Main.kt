e3008dcac9856acd969ab65f68267a28facc4eaf
Alzhikkkk <alzhikakh@gmail.com>
2022-09-10 13:59:03 +0600

    Sequence

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,8 +4,8 @@ fun main() {
     val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
 
     // eager, creates a new list
-    val eager = decorations.filter { it [0] == 'p' }
-    println("eager: $eager")
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("filtered: $filtered")
 }
 
 
