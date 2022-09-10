5a29c7f5c7c289493327d2df5bab439ea9c7b7b5
Alzhikkkk <alzhikakh@gmail.com>
2022-09-10 14:05:54 +0600

    Collection of collections flatten()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,26 +1,12 @@
 import java.util.*;
 
 fun main() {
-    val decorations = listOf ("rock", "pagoda", "plastic plant", "alligator", "flowerpot")
-
-    // eager, creates a new list
-    val lazyMap = decorations.asSequence().map {
-        println("access: $it")
-        it
-    }
-
-    println("lazy: $lazyMap")
+    val mysports = listOf("basketball", "fishing", "running")
+    val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+    val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+    val mylist = listOf(mysports, myplayers, mycities)     // list of lists
     println("-----")
-    println("first: ${lazyMap.first()}")
-    println("-----")
-    println("all: ${lazyMap.toList()}")
-
-    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
-        println("access: $it")
-        it
-    }
-    println("-----")
-    println("filtered: ${lazyMap2.toList()}")
+    println("Flat: ${mylist.flatten()}")
 }
 
 
