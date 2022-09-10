e8d64439c913bc6b75d36b00c7e797417176c286
Alzhikkkk <alzhikakh@gmail.com>
2022-09-10 13:49:44 +0600

    Adding required parameter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,39 @@
-fun main(args: Array<String>) {
-    swim()   // uses default speed
-    swim("slow")   // positional argument
-    swim(speed="turtle-like")   // named parameter
+import java.util.*;
+fun fishFood (day : String) : String {
+    val food : String
+    when (day) {
+        "Monday" -> food = "flakes"
+        "Wednesday" -> food = "redworms"
+        "Thursday" -> food = "granules"
+        "Friday" -> food = "mosquitoes"
+        "Sunday" -> food = "plankton"
+        else -> food = "nothing"
+    }
+    return food
 }
 
-fun swim(speed: String = "fast") {
-    println("swimming $speed")
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println ("Today is $day and the fish eat $food")
+    println("Change water: ${shouldChangeWater(day)}")
+}
+
+fun main(args: Array<String>) {
+    feedTheFish()
+}
+
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+}
+
+fun shouldChangeWater (day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+    return when {
+        temperature > 30 -> true
+        dirty > 30 -> true
+        day == "Sunday" ->  true
+        else -> false
+    }
 }
