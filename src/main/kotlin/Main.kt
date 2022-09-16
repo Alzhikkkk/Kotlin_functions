3ecccf97d31a9b44c60c98f5721166a7e5498439
Alzhikkkk <alzhikakh@gmail.com>
2022-09-16 11:10:10 +0600

    Adding getter

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,7 +4,6 @@ import java.util.*;
 fun buildAquarium() {
     val aquarium6 = Aquarium(numberOfFish = 29)
     aquarium6.printSize()
-    println("Volume: ${aquarium6.width * aquarium6.length * aquarium6.height / 1000} liters")
 }
 
 fun main() {
