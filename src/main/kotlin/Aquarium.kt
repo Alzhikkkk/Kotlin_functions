1437beb2becd668f2d138ec93eb63becc8a69c84
Alzhikkkk <alzhikakh@gmail.com>
2022-09-16 11:21:16 +0600

    Adding a subclass

--- src/main/kotlin/Aquarium.kt
+++ src/main/kotlin/Aquarium.kt
@@ -1,6 +1,6 @@
 package example.myapp
 
-class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+open class Aquarium (open var length: Int = 100, open var width: Int = 20, open var height: Int = 40) {
     init {
         println("aquarium initializing")
     }
@@ -10,18 +10,23 @@ class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40)
         height = (tank / (length * width)).toInt()
     }
 
-    var volume: Int
-        get() = width * height * length / 1000  // 1000 cm^3 = 1 liter
+    open var volume: Int
+        get() = width * height * length / 1000
         set(value) {
             height = (value * 1000) / (width * length)
         }
 
+    open val shape = "rectangle"
+
+    open var water: Double = 0.0
+        get() = volume * 0.9
+
     fun printSize() {
+        println(shape)
         println("Width: $width cm " +
                 "Length: $length cm " +
-                "Height: $height cm "
-        )
-        // 1 liter = 1000 cm^3
-        println("Volume: $volume liters")
+                "Height: $height cm ")
+        // 1 l = 1000 cm^3
+        println("Volume: $volume liters Water: $water liters (${water / volume * 100.0}% full)")
     }
 }
