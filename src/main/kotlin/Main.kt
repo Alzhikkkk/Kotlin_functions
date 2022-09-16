e2776fb39cf475bb122a6a244ee0f68c60e7a700
Alzhikkkk <alzhikakh@gmail.com>
2022-09-16 11:56:31 +0600

    Adding enum class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -21,8 +21,13 @@ fun makeFish() {
 }
 
 
-fun main() {
-    decor.makeDecorations()
+enum class Direction(val degrees: Int) {
+    NORTH(0), SOUTH(180), EAST(90), WEST(270)
 }
 
+fun main() {
+    println(Direction.EAST.name)
+    println(Direction.EAST.ordinal)
+    println(Direction.EAST.degrees)
+}
 
